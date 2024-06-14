ed485b1012749f990e4f4350903089951ab9263d
Austyn Whaley <austyn_whaley@yahoo.com>
2024-06-14 09:17:21 -0400

    p1b solved

--- p01/ps1b.py
+++ p01/ps1b.py
@@ -15,16 +15,19 @@ semi_annual_raise = float(input("Enter the semi­annual raise, as a decimal: "))
 portion_down_payment = 0.25 * total_cost
 current_savings = 0.0
 r = 0.04
-
-monthly_income = annual_salary / 12
-
 months = 0
 
+
 while current_savings < portion_down_payment:
+        
+    monthly_income = annual_salary / 12
     monthly_return = current_savings * r / 12
     monthly_savings = monthly_income * portion_saved
     current_savings += monthly_savings + monthly_return 
     months += 1
+    
+    if months % 6 == 0:
+        annual_salary += annual_salary * semi_annual_raise
 
 
 
